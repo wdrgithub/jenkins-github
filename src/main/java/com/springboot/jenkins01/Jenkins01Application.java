ab088b90ef11925469e03897b6e9f72d3250a548
origin <https://github.com/wdrgithub/jenkins-github.git>
2020-03-09 16:31:20 +0800

    通过idea将springboot项目推送到github上

--- src/main/java/com/springboot/jenkins01/Jenkins01Application.java
+++ src/main/java/com/springboot/jenkins01/Jenkins01Application.java
@@ -3,6 +3,9 @@ package com.springboot.jenkins01;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * springboot主程序方法
+ */
 @SpringBootApplication
 public class Jenkins01Application {
 
