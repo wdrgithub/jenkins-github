4f4b954641e99948297c475c8ff9a446cc4b2ab4
origin <https://github.com/wdrgithub/jenkins-github.git>
2020-03-10 11:56:18 +0800

    test

---
+++ src/main/java/com/jenkins/jenkinsspringbootgithubauto/JenkinsSpringbootGithubAutoApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.jenkinsspringbootgithubauto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsSpringbootGithubAutoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsSpringbootGithubAutoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jenkins/jenkinsspringbootgithubauto/JenkinsSpringbootGithubAutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.jenkinsspringbootgithubauto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsSpringbootGithubAutoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
